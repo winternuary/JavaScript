b3c4d59f51e7f024aeb96cae7e960c98b8893667
youngeunkim <pkhj286@gmail.com>
2023-07-22 10:22:00 +0900

    최고기록 일단...

--- popchu/script.js
+++ popchu/script.js
@@ -1,10 +1,21 @@
 // JavaScript 코드
 const pop = document.getElementById("close");
 const scoreElement = document.getElementById("score");
+const legendElement = document.getElementById("legend");
 
 let showScore = 0;
+let showLegend = parseInt(localStorage.getItem("showLegend")) || 0; // 최고 기록을 숫자로 변환
 let popClick = false;
 let originalSrc = pop.src;
+
+const recordHighScore = () => {
+  if (showScore > showLegend) {
+    // showScore와 최고 기록을 비교
+    showLegend = showScore;
+    legendElement.textContent = showLegend; // 최고 기록 표시 업데이트
+    localStorage.setItem("showLegend", showLegend.toString()); // 최고 점수 로컬 스토리지에 저장하기 (숫자로 변환하여 저장)
+  }
+};
 
 // 로컬 스토리지에서 저장된 score 가져오기
 const savedScore = localStorage.getItem("score");
@@ -27,6 +38,8 @@ const popchu = () => {
 
     // score 업데이트 후 로컬 스토리지에 저장
     saveScore();
+
+    recordHighScore();
   }
 };
 
